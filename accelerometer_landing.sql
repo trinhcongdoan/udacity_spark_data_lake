f0317899bdfba3036f82bf7d54c65b3bd5fcd1c2
trinhcongdoan <70443420+trinhcongdoan@users.noreply.github.com>
2023-05-10 23:06:00 +0700

    Update accelerometer_landing.sql

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,1 +1,27 @@
-
+CREATE EXTERNAL TABLE `accelerometer`(
+  `user` string COMMENT 'from deserializer', 
+  `timestamp` bigint COMMENT 'from deserializer', 
+  `x` double COMMENT 'from deserializer', 
+  `y` double COMMENT 'from deserializer', 
+  `z` double COMMENT 'from deserializer')
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='timeStamp,user,x,y,z') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://udacity-project-spark-and-data-lake/data/accelerometer/'
+TBLPROPERTIES (
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='crawler_accelerometer', 
+  'averageRecordSize'='85', 
+  'classification'='json', 
+  'compressionType'='none', 
+  'objectCount'='1', 
+  'recordCount'='7', 
+  'sizeKey'='601', 
+  'typeOfData'='file')
