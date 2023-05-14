23fd20cdbc54387b1473cff3d6cff7e132537833
trinhcongdoan <70443420+trinhcongdoan@users.noreply.github.com>
2023-05-14 09:12:17 +0700

    Update customer_trusted.py

--- customer_trusted.py
+++ customer_trusted.py
@@ -4,6 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+import re
 
 args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
@@ -12,8 +13,8 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args["JOB_NAME"], args)
 
-# Script generated for node S3 bucket
-S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
+# Script generated for node Amazon S3
+AmazonS3_node1684027402781 = glueContext.create_dynamic_frame.from_options(
     format_options={"multiline": False},
     connection_type="s3",
     format="json",
@@ -21,37 +22,22 @@ S3bucket_node1 = glueContext.create_dynamic_frame.from_options(
         "paths": ["s3://udacity-project-spark-and-data-lake/data/customers/"],
         "recurse": True,
     },
-    transformation_ctx="S3bucket_node1",
+    transformation_ctx="AmazonS3_node1684027402781",
 )
 
-# Script generated for node ApplyMapping
-ApplyMapping_node2 = ApplyMapping.apply(
-    frame=S3bucket_node1,
-    mappings=[
-        ("customerName", "string", "customerName", "string"),
-        ("email", "string", "email", "string"),
-        ("phone", "string", "phone", "string"),
-        ("birthDay", "string", "birthDay", "string"),
-        ("serialNumber", "string", "serialNumber", "string"),
-        ("registrationDate", "bigint", "registrationDate", "long"),
-        ("lastUpdateDate", "bigint", "lastUpdateDate", "long"),
-        ("shareWithResearchAsOfDate", "bigint", "shareWithResearchAsOfDate", "long"),
-        ("shareWithPublicAsOfDate", "bigint", "shareWithPublicAsOfDate", "long"),
-        ("shareWithFriendsAsOfDate", "bigint", "shareWithFriendsAsOfDate", "long"),
-    ],
-    transformation_ctx="ApplyMapping_node2",
+# Script generated for node Filter
+Filter_node1684027445179 = Filter.apply(
+    frame=AmazonS3_node1684027402781,
+    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
+    transformation_ctx="Filter_node1684027445179",
 )
 
-# Script generated for node S3 bucket
-S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
-    frame=ApplyMapping_node2,
-    connection_type="s3",
-    format="glueparquet",
-    connection_options={
-        "path": "s3://udacity-project-spark-and-data-lake/data/customer_trusted/",
-        "partitionKeys": [],
-    },
-    transformation_ctx="S3bucket_node3",
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1684029224267 = glueContext.write_dynamic_frame.from_catalog(
+    frame=Filter_node1684027445179,
+    database="humanba",
+    table_name="customer_trusted",
+    transformation_ctx="AWSGlueDataCatalog_node1684029224267",
 )
 
 job.commit()
